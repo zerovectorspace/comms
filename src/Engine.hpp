fdaf9f5e46b8df03119343a8b0281ccaa8d5dfff
z y <zachary_young@zerovector.space>
2019-02-20 22:42:58 -0800

    More reorganizing

--- src/Engine.hpp
+++ src/Engine.hpp
@@ -1,7 +1,7 @@
 #ifndef COMMS_ENGINE_HEADER
 #define COMMS_ENGINE_HEADER
 
-#include "Comms/Commands.hpp"
+#include "comms/Commands.hpp"
 
 /********************************************************************************
  *                                   Engine
