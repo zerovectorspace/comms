fec794f619f6f35040837328ea07cbed4eceb4c3
Zachary Young <zyoung@berkeley.edu>
2020-01-28 18:28:42 -0800

    Remove control flow diagram from main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,20 +29,6 @@
  *
  */
 
-/*
- * Program Flow
- * ------------
- *                                                                         __.-> Exec     -> Command Functors
- *                                                                       /`__.-> Try_Exec -> Command Functors
- * Engine<Init> -> Engine<Main_Loop> -> Engine<Parse_Args> -> Command<Run>`
- *       \                          `-> Engine<Parse_Args> -> Command<Run> -> Try_Exec -> Command Functors
- *        \                                                               `-> Exec     -> Command Functors
- *         \
- *          `Engine<Net_Loop> -> Engine<Parse_Buffer> -> Command<Run> -> Net Comm Map -> Exec    -> Command Functors
- *                                                                                  `-> Try_Exec -> Command Functors
- *                                                   
- */
-
 auto help( char const* name )
 {
   fct::print( "Usage:", ' ' );
