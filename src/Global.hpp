b574fbd4a33d31c83719b80719ecdb2603c0cb69
z <zachary_young@zerovector.space>
2020-03-16 23:24:10 -0700

    Add documentation to Global.hpp

--- src/Global.hpp
+++ src/Global.hpp
@@ -11,48 +11,108 @@ namespace Comms
   using Char_Texture_Map = std::unordered_map<Char, Character>;
   using VWindow_Map      = std::unordered_map<StdStr, VWindow>;
 
+  /**
+   * Used to distiguish between Application type
+   *
+   * Local Server -> interacts with remote servers
+   * Local Client -> the gui for user interaction
+   */
   enum App {
     SERVER,
     CLIENT
   };
 
+  /**
+   * Global application state
+   */
   struct Global
   {
     App app = App::SERVER;
 
-    /***
-     * Command maps
+    /**
+     * Local commands to interact with
+     *   the Local Client
      */
     Command_Map lcl_comms{};
-    Command_Map win_comms{};
+
+    /**
+     * Protocol to handle interactions
+     *   between the Local Server/Client
+     */
     Proto_Map proto_local{};
+
+    /**
+     * Protocol to handle interactions
+     *   between the Remote Server/Server
+     */
     Proto_Map proto_remote{};
 
-    /***
+    /**
      * Peer_Map :: Handle to `Peer`s
      */
     Peer_Map peer_map{};
 
-    /***
+    /**
      * Unix Socket
      */
     Vec<pollfd> poll_fds{};
+
+    /**
+     * Socket number for the
+     *   Local Server/Client
+     */
     Socket_Num socket_unix = -1;
+
+    /**
+     * Socket file name
+     */
     String socket_unix_filename = "comms.socket"_s;
+
+    /**
+     * Socket number for the
+     *   Server UDP listen socket
+     */
     Socket_Num socket_udp{};
 
-    /***
-     * OpenGL
+    /**
+     * Local Client window dimensions
      */
     Int win_w = 800;
     Int win_h = 600;
+
+    /**
+     * SDL Window for the Local Client
+     */
     SDL_Window*   win = nullptr;
+
+    /**
+     * SDL Renderer for the Local Client
+     */
     SDL_Renderer* ren = nullptr;
+
+    /**
+     * Handle to the OpenGL program
+     */
     UInt gl_prog{};
+
+    /**
+     * Handle to the OpenGL Vertex Array Object
+     */
     UInt text_VAO{};
+
+    /**
+     * Handle to the OpenGL Vertex Buffer Object
+     */
     UInt text_VBO{};
 
+    /**
+     * Semaphore to application running state
+     */
     bool is_running = true;
+
+    /**
+     * Semaphore to redraw entire window
+     */
     bool redraw = false;
 
     /***
@@ -69,35 +129,33 @@ namespace Comms
 
     Char_Texture_Map chrs;
 
-    /***
-     * Buffer :: Holds all text
-     */
-    Vec<String> bufs = { String{} };
-    String* buf = &bufs.back();
-
-    /***
+    /**
      * Command prompt
      */
     String cmd_prompt = "cmd> "_s;
 
-    /***
+    /**
      * Input mode
      */
     MODE mode = MODE::Text_Input;
 
-    /***
+    /**
      * Cursor
      */
     Cursor curs{};
 
-    /***
-     * VWins :: Holds all VWindows
+    /**
+     * Holds all VWindows
      */
     VWindow_Map vwins{};
+    
+    /**
+     * Handle to currently viewed VWindow
+     */
     VWindow* vwin = nullptr;
   };
 
-  /***
+  /**
    * Global data object
    */
   Global _g{};
